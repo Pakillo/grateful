d4e87bab0670ef9b56f48e8e841a7367b1a73f79
Pakillo <f.rodriguez.sanc@gmail.com>
2017-08-09 02:10:01 +0200

    add fun to fetch csl from internet

---
+++ R/get_csl.R
@@ -0,0 +1,17 @@
+#' Get a journal citation style from internet repository
+#'
+#' @param name Name of the journal, exactly as in \url{https://github.com/citation-style-language/styles}.
+#'
+#' @return The CSL file is saved in the working directory
+#' @export
+#'
+#' @examples
+#' library(grateful)
+#' get_csl("ecosistemas")
+get_csl <- function(name) {
+
+  styles.repo <- "https://raw.githubusercontent.com/citation-style-language/styles/master/"
+
+  download.file(paste0(styles.repo, name, ".csl"), mode = "wb",
+                destfile = file.path(getwd(), paste0(name, ".csl")))
+}
