4efd731b996c383ba831ac9bf4079bb3cd853dff
Connor P. Jackson <cpjackson@berkeley.edu>
2019-07-22 21:36:09 -0700

    Add a citation for RStudio when used interactively
    Also be more flexible with capitalization of "rmd"

--- R/cite_packages.R
+++ R/cite_packages.R
@@ -7,6 +7,8 @@
 #' @param style Optional. Citation style to format references. See \url{http://citationstyles.org/styles/}.
 #' @param out.format Output format, either "docx" (Word), "pdf", "html", or "md" (markdown).
 #' @param out.dir Directory to save the output document and a BibTeX file with the references. Defaults to working directory.
+#' @param include.RStudio Logical. If TRUE, adds a citation for the current version of RStudio, if run within RStudio interactively.
+#'
 #' @param ... Other parameters passed to \code{\link[checkpoint]{scanForPackages}}.
 #'
 #' @return A document with rendered citations in the desired format, plus a file ("pkg-refs.bib") containing references in BibTeX format.
@@ -19,12 +21,12 @@
 #' cite_packages(style = "ecology", out.format = "docx")
 #' }
 cite_packages <- function(all.pkg = TRUE, include.rmd = TRUE, style = NULL,
-                          out.format = "html", out.dir = getwd(), ...) {
+                          out.format = "html", out.dir = getwd(), include.RStudio = FALSE, ...) {
   pkgs <- scan_packages(all.pkgs = all.pkg, include.Rmd = include.rmd, ...)
-  cites <- get_citations(pkgs, out.dir = out.dir) # produces "pkg-refs.bib" file
+  cites <- get_citations(pkgs, out.dir = out.dir, includeRStudio = include.RStudio) # produces "pkg-refs.bib" file
   rmd <- create_rmd(cites, csl = style, out.dir = out.dir) # produces "refs.Rmd"
 
-  if (out.format == "rmd" | out.format == "Rmd") {
+  if (tolower(out.format) == "rmd") {
     return(rmd) # Keep the rmarkdown file and return the file object
   }
   else {
--- R/get_citations.R
+++ R/get_citations.R
@@ -3,6 +3,7 @@
 #' @param pkgs Character vector of package names, e.g. as produced by \code{scan_packages}.
 #' @param filename Optional. Name of BibTeX file containing packages references.
 #' @param out.dir Directory to save the BibTeX file with the references. Defaults to working directory.
+#' @param includeRStudio Logical. If TRUE, adds a citation for the current version of RStudio, if run within RStudio interactively.
 #'
 #' @return Nothing by default. If assigned a name, a list with citation keys for each citation (without). Optionally, a file with references in BibTeX format.
 #' @export
@@ -13,10 +14,14 @@
 #' pkgs <- scan_packages()
 #' cites <- get_citations(pkgs)
 #' }
-get_citations <- function(pkgs, filename = "pkg-refs.bib", out.dir = getwd()) {
+get_citations <- function(pkgs, filename = "pkg-refs.bib",
+                          out.dir = getwd(), includeRStudio = FALSE) {
 
   cites.bib <- lapply(cit.list, get_citation_and_citekey)
 
+  if (includeRStudio == TRUE) {
+    # Put an RStudio citation on the end
+    cites.bib[[length(cites.bib) + 1]] <- add_citekey("rstudio", RStudio.Version()$citation)
   }
 
   ## write bibtex references to file
