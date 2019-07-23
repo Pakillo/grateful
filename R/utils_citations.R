da88e83d041b7732533a7650694b115ec370a9a0
Connor P. Jackson <cpjackson@berkeley.edu>
2019-07-22 21:32:04 -0700

    Handle multiple citations with years and then letters

--- R/utils_citations.R
+++ R/utils_citations.R
@@ -1,12 +1,17 @@
-# for a name and citation, cretae unique citekey for each citation
+# for a name and citation, create unique citekey for each citation
 add_citekey <- function(pkg_name, citation) {
   citation_bibtex <- utils::toBibtex(citation)
   refbeginnings <- grep(pattern = "\\{,$", x = citation_bibtex)
   for (i in 1:length(refbeginnings)) {
     if (length(refbeginnings) == 1) {
+      # only one citation, citekey = package name
       replaceString <- paste0("{", pkg_name, ",")
+    } else if (length(unique(unlist(citation$year))) == length(unlist(citation$year))) {
+      # multiple citations, unique years. citekey = packagename + year
+      replaceString <- paste0("{", pkg_name, citation[[i]]$year, ",")
     } else {
-      replaceString <- paste0("{", pkg_name, i, ",")
+      # multiple citations, duplicate years. citekey = packagename + year + letter
+      replaceString <- paste0("{", pkg_name, citation[[i]]$year, letters[i], ",")
     }
     citation_bibtex[refbeginnings[i]] <- sub(pattern = "\\{,$", replacement = replaceString,
                                              x = citation_bibtex[refbeginnings[i]])
@@ -14,7 +19,9 @@ add_citekey <- function(pkg_name, citation) {
   return(citation_bibtex)
 }
 
-# For a package, get the citation, then pass along the name and citation
+# For a package, get the citation, remove special characters from the name,
+# then pass along the name and citation
 get_citation_and_citekey <- function(pkg_name) {
-  return(add_name(pkg_name, utils::citation(pkg_name)))
+  pkgname_clean <- gsub("[^[:alnum:]]", "", pkg_name)
+  return(add_citekey(pkgname_clean, utils::citation(pkg_name)))
 }
