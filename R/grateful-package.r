aa547588ad5d098635a9efb055791fc1e0f47d3a
Pakillo <f.rodriguez.sanc@gmail.com>
2021-05-03 14:23:07 +0200

    dontrun get_csl example

--- R/grateful-package.r
+++ R/grateful-package.r
@@ -1,5 +1,11 @@
 #' grateful.
 #'
+#' The goal of `grateful` is to make it very easy to cite the R packages used in any report or publication. By calling a single function, it will scan the project for R packages used, and generate a document with citations in the desired output format (Word, PDF, HTML, Markdown).
+#'
+#' Importantly, these references can be formatted for a specific journal so that we can just paste them directly into the bibliography list of our manuscript or report.
+#'
+#' If you prefer to get just a BibTeX file with all package citations, it can also be done (see examples in https://github.com/Pakillo/grateful).
+#'
 #' @name grateful
 #' @docType package
 NULL
