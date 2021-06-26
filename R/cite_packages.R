ffab4f171b9c921d5bff2ee2cfdb7021a7c7989d
Pakillo <f.rodriguez.sanc@gmail.com>
2021-06-26 13:01:26 +0200

    switch to renv for scanning dependencies. See #24

--- R/cite_packages.R
+++ R/cite_packages.R
@@ -2,14 +2,18 @@
 #'
 #' This is a wrapper function.
 #'
-#' @param all.pkg Logical. Include all packages used in scripts within the project/folder (the default), or only packages used in the current session? If TRUE, uses \code{\link[checkpoint]{scanForPackages}}, otherwise uses \code{\link[utils]{sessionInfo}}.
-#' @param include.rmd Logical. Include packages used in Rmarkdown documents? (default is TRUE, requires \code{knitr} package).
+#' @param all.pkg Logical. Include all packages used in scripts within the
+#' project/folder (the default), or only packages used in the current session?
+#' If TRUE, uses \code{\link[renv]{dependencies}},
+#' otherwise uses \code{\link[utils]{sessionInfo}}.
 #' @param style Optional. Citation style to format references. See \url{https://www.zotero.org/styles}.
-#' @param out.format Output format, either "docx" (Word), "pdf", "html", or "md" (markdown).
-#' @param out.dir Directory to save the output document and a BibTeX file with the references. Defaults to working directory.
-#' @param ... Other parameters passed to \code{\link[checkpoint]{scanForPackages}}.
+#' @param out.format Output format, either "docx", "pdf", "html", or "Rmd" (Rmarkdown).
+#' @param out.dir Directory to save the output document and a BibTeX file with the references.
+#' Defaults to working directory.
+#' @param ... Other parameters passed to \code{\link[renv]{dependencies}}.
 #'
-#' @return A document with rendered citations in the desired format, plus a file ("pkg-refs.bib") containing references in BibTeX format.
+#' @return A document with rendered citations in the desired format,
+#' plus a file ("pkg-refs.bib") containing references in BibTeX format.
 #' @export
 #'
 #' @examples
@@ -18,9 +22,12 @@
 #' cite_packages()
 #' cite_packages(style = "ecology", out.format = "docx")
 #' }
-cite_packages <- function(all.pkg = TRUE, include.rmd = TRUE, style = NULL,
-                          out.format = "html", out.dir = getwd(), ...) {
-  pkgs <- scan_packages(all.pkgs = all.pkg, include.Rmd = include.rmd, ...)
+cite_packages <- function(all.pkg = TRUE,
+                          style = NULL,
+                          out.format = "html",
+                          out.dir = getwd(),
+                          ...) {
+  pkgs <- scan_packages(all.pkgs = all.pkg, ...)
   cites <- get_citations(pkgs, out.dir = out.dir) # produces "pkg-refs.bib" file
   rmd <- create_rmd(cites, csl = style, out.dir = out.dir) # produces "refs.Rmd"
 
--- R/scan_packages.R
+++ R/scan_packages.R
@@ -1,21 +1,21 @@
 #' Scan a project or folder for packages used
 #'
-#' @param all.pkgs Logical. Include all packages used in scripts within the project/folder (the default), or only packages used in the current session? If TRUE, uses \code{\link[checkpoint]{scanForPackages}}, otherwise uses \code{\link[utils]{sessionInfo}}.
-#' @param include.Rmd Logical. Include packages used in Rmarkdown documents? (default is TRUE, requires \code{knitr} package).
-#' @param ... Other parameters passed to \code{\link[checkpoint]{scanForPackages}}.
+#' @param all.pkgs Logical. Include all packages used in scripts within
+#' the project/folder (the default), or only packages used in the current session?
+#' If TRUE, uses \code{\link[renv]{dependencies}},
+#' otherwise uses \code{\link[utils]{sessionInfo}}.
+#' @param ... Other parameters passed to \code{\link[renv]{dependencies}}.
 #'
 #' @return a character vector of package names
 #' @export
-#' @importFrom checkpoint scanForPackages
-#' @import utils
 #'
 #' @examples
 #' library(grateful)
 #' scan_packages()
-scan_packages <- function(all.pkgs = TRUE, include.Rmd = FALSE, ...) {
+scan_packages <- function(all.pkgs = TRUE, ...) {
 
   if (all.pkgs) {
-    pkgs <- checkpoint::scanForPackages(use.knitr = include.Rmd, ...)$pkgs
+    pkgs <- unique(renv::dependencies(...)$Package)
   } else {
     pkgs <- names(utils::sessionInfo()$otherPkgs)
   }
