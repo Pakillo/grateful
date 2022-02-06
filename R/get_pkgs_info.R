d4d3292dbefa7002167f7e8aaf7b03b585967f8b
Pakillo <f.rodriguez.sanc@gmail.com>
2022-02-06 23:03:13 +0100

    add new fun to produce table with pkg info

---
+++ R/get_pkgs_info.R
@@ -0,0 +1,40 @@
+
+#' Get information about packages used in a project
+#'
+#' This function scans the project for R packages used and returns a data frame
+#' with package names, version, and citation keys.
+#'
+#' @inheritParams scan_packages
+#' @inheritParams get_citations
+#' @inheritDotParams scan_packages
+#'
+#' @return A data.frame with package info,
+#' and a file containing package references in BibTeX format.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_pkgs_info()
+#' }
+
+get_pkgs_info <- function(all.pkgs = TRUE,
+                          out.dir = getwd(),
+                          bibfile = "grateful-refs.bib",
+                          include.RStudio = FALSE,
+                          ...) {
+
+  pkgs.df <- scan_packages(all.pkgs = all.pkgs, ...)
+
+  citekeys <- get_citations(pkgs.df$pkg,
+                            out.dir = out.dir,
+                            bibfile = bibfile,
+                            include.RStudio = include.RStudio)
+
+  # Group all citations from same package
+  pkgs.df$citekeys <- lapply(pkgs.df$pkg, function(x) {
+    citekeys[grep(x, citekeys)]
+  })
+
+  pkgs.df
+
+}
