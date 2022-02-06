49433bce724c631ce92726952db27c80dba672ac
Pakillo <f.rodriguez.sanc@gmail.com>
2022-02-06 23:17:53 +0100

    new funs to write paragraph citing R, packages, versions...

---
+++ R/write_citation_paragraph.R
@@ -0,0 +1,43 @@
+write_citation_paragraph <- function(df, include.RStudio = FALSE) {
+
+  df.pkgs <- df[df$pkg != "base", ]
+
+  parag <- paste0(
+    "We used R version ", get_version("base", df), " ", get_citekeys("base", df),
+    " and the following R packages: ",
+    paste(format_pkg_citation(pkgname = df.pkgs$pkg, df), collapse = ", ")
+  )
+
+  if (include.RStudio) {
+    parag <- paste0(parag, ", running in RStudio v. ", rstudioapi::versionInfo()$version,
+                    " [@rstudio].")
+  } else {
+    parag <- paste0(parag, ".")
+  }
+
+  parag
+}
+
+
+get_version <- function(pkgname, df) {
+  df$version[df$pkg == pkgname]
+}
+
+get_citekeys <- function(pkgname, df) {
+  citekeys <- sort(unlist(df$citekeys[df$pkg == pkgname], use.names = FALSE))
+  citk <- paste0("@", citekeys)
+  citk <- paste(citk, collapse = "; ")
+  citks <- paste0("[", citk, "]")
+}
+
+format_pkg_citation <- Vectorize(
+
+  function(pkgname, df) {
+
+    pkginfo <- paste(pkgname,
+                     "v.", get_version(pkgname, df = df),
+                     get_citekeys(pkgname, df = df))
+
+  },
+
+  vectorize.args = "pkgname")
