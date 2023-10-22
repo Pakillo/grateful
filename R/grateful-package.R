eb593b1df937e59229ab87982e1f00a5f49a5e21
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-10-22 14:31:24 +0200

    update package doc cf. https://github.com/r-lib/roxygen2/issues/1491

--- R/grateful-package.R
+++ R/grateful-package.R
@@ -1,9 +1,6 @@
-#' Grateful: Facilitate citation of R Packages
-#'
-#' Find R packages used in a project, create a BibTeX file of citations, and
-#' cite them in either a new or existing document. See the primary function
-#' [cite_packages()] for full details and usage instructions.
-#'
-#' @name grateful
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
