48a76a0c24c309be43c0562a18b7edb67214add9
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2025-11-04 10:30:56 +0100

    fix test

--- tests/testthat/test-cite_packages.R
+++ tests/testthat/test-cite_packages.R
@@ -96,8 +96,9 @@ test_that("cite_packages returns correct paragraph", {
                         pkgs = c("Depends", "Imports", "Suggests"),
                         desc.path = desc,
                         out.dir = tempdir())
-  expect_identical(para,
-                   structure("We used R v. >= 3.0.0 [@base] and the following R packages: curl [@curl], desc [@desc], knitr [@knitr2014; @knitr2015; @knitr2025], remotes [@remotes], renv [@renv], rmarkdown [@rmarkdown2018; @rmarkdown2020; @rmarkdown2024], rstudioapi [@rstudioapi], testthat v. >= 3.0.0 [@testthat], utils [@utils].", class = "knit_asis", knit_cacheable = NA))
+
+  expect_identical(gsub("\\s\\[.*?\\]", "", para),  # remove citation keys, which may change
+                   structure("We used R v. >= 3.0.0 and the following R packages: curl, desc, knitr, remotes, renv, rmarkdown, rstudioapi, testthat v. >= 3.0.0, utils.", class = "knit_asis", knit_cacheable = NA))
 
 
 })
